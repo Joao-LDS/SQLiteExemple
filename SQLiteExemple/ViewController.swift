3047636c650e3d864dbbe815de69b2f5998c2196
João <joao@Joaos-iMac-Pro.local>
2020-11-02 08:06:05 -0300

    First commit

---
+++ SQLiteExemple/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SQLiteExemple
+//
+//  Created by João on 02/11/20.
+//  Copyright © 2020 João. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
